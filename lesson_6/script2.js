8ac496db1bc64624484a72e3e29701606803f2c0
Illia Lytvynov <illial@wix.com>
2023-11-01 21:04:23 +0200

    Lesson 6

---
+++ lesson_6/script2.js
@@ -0,0 +1,1 @@
+const modalContainer = 'hello';
---
+++ lesson_6/scripts.js
@@ -0,0 +1,34 @@
+function popup() {
+  const modalContainer = document.querySelector('.modal');
+  const toggleButton = document.querySelector('.toggle');
+  const closeButton = document.querySelector('.modal__close');
+  const initialToggleText = toggleButton.textContent;
+  let intervalId = null;
+  
+  function openModal() {
+    modalContainer.classList.add('modal__visible');
+    toggleButton.textContent = 'Opened!!!';
+    intervalId = setInterval(() => {
+      console.log('asdfasdfasd')
+    }, 3000);
+  }
+
+
+  function closeModal() {
+    modalContainer.classList.remove('modal__visible');
+    toggleButton.textContent = initialToggleText;
+    clearInterval(intervalId);
+  }
+
+  
+
+  toggleButton.addEventListener('click', openModal);
+  closeButton.addEventListener('click', closeModal);
+}
+
+popup();
+
+
+
+
+
